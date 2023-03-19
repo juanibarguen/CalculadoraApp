77e00d9a8c75240ddcb9154c05348dfcda0a5d6c
juanibarguen <h===>
2023-03-19 17:16:49 -0300

    Commit inicial

--- CalculadoraApp/ViewController.swift
+++ CalculadoraApp/ViewController.swift
@@ -9,11 +9,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var firstNumber: Double = 0
+    var operation: String = ""
+    var isTypingNumber: Bool = false
+    var decimalPoint: Bool = false // Variable que indica si ya se ingresó un punto decimal
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        let number = String(sender.tag)
+        
+        if isTypingNumber {
+            displayLabel.text = displayLabel.text! + number
+        } else {
+            displayLabel.text = number
+            isTypingNumber = true
+        }
+    }
+    
+    @IBAction func decimalButtonPressed(_ sender: UIButton) {
+        
+        // Evaluamos que el numero en pantalla no contenga ya un "."
+        if ((displayLabel.text?.contains(".")) != nil) {
+            decimalPoint = false
+        } else {
+            
+            if !decimalPoint {
+                displayLabel.text = displayLabel.text! + "."
+                decimalPoint = true
+                isTypingNumber = true
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func operationButton(_ sender: UIButton) {
+        operation = String(sender.tag)
+        firstNumber = Double(displayLabel.text!)!
+        isTypingNumber = false
+        decimalPoint = false
+    }
+    
+    @IBAction func resultButton(_ sender: UIButton) {
+        let secondNumber = Double(displayLabel.text!)!
+        var result: Double = 0
+        
+        switch operation {
+        
+        case "10":
+            result = firstNumber - secondNumber
+        case "20":
+            result = firstNumber + secondNumber
+        case "30":
+            result = firstNumber * secondNumber
+        case "40":
+            result = firstNumber / secondNumber
+        case "50":
+            result = firstNumber * (secondNumber / 100)
+        default:
+            break
+        }
+        
+        
+        if result.truncatingRemainder(dividingBy: 1) == 0 {
+            // El resultado es entero, se muestra sin decimales
+            displayLabel.text = ("\(Int(result))")
+        } else {
+            // El resultado no es entero, se muestra con dos decimales
+            displayLabel.text = ("\(String(format: "%.2f", result))")
+        }
+        
+        isTypingNumber = false
+        decimalPoint = false
+        
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        displayLabel.text = "0"
+        isTypingNumber = false
+        firstNumber = 0
+        operation = ""
+        decimalPoint = false
+    }
 }
+
+
+
 
